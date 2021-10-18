b653549745025afd03dc491a745f1717dea54d56
richasingh-92 <91095253+richasingh-92@users.noreply.github.com>
2021-10-18 15:20:08 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,9 @@
+
+alert ("Welcome \n It's my first responsive website. \nSo please ignore the design my main motive is to make it responsive :)")
+
+
+alert ("Any suggestion will be appericiated\n THANKYOU")
+
+
+
+    
